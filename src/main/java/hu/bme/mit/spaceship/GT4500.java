23573c902a2edc861a68299413a733b0f5128fb9
Sipor <bencike.csaki@gmail.com>
2022-04-08 09:14:58 +0200

    Implemented firing_All mode

--- src/main/java/hu/bme/mit/spaceship/GT4500.java
+++ src/main/java/hu/bme/mit/spaceship/GT4500.java
@@ -79,6 +79,13 @@ public class GT4500 implements SpaceShip {
       case ALL:
         // try to fire both of the torpedo stores
         //TODO implement feature
+        if(!primaryTorpedoStore.isEmpty() && !secondaryTorpedoStore.isEmpty()){
+          boolean first = primaryTorpedoStore.fire(1);
+          boolean second = secondaryTorpedoStore.fire(1);
+          if(first && second){
+            firingSuccess = true;
+          }
+        }
 
         break;
     }
